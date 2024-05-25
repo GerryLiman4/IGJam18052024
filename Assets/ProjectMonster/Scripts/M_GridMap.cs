d3e949e0f6329b360a355b6c0632b245ff745283
Andrew <andrewliwanto348@yahoo.co.id>
2024-05-25 15:44:56 +0700

    Commit projectmonster scripts folder

---
+++ Assets/ProjectMonster/Scripts/M_GridMap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_GridMap : MonoBehaviour
+{
+
+
+	public void Initialize(Vector2Int size)
+	{
+
+	}
+
+}

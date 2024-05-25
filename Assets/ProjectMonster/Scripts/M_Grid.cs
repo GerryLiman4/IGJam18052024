a65126cc7cd518d046e9e70d96279c6e9e76665d
Andrew <andrewliwanto348@yahoo.co.id>
2024-05-25 16:53:37 +0700

    Added gridmap

--- Assets/ProjectMonster/Scripts/M_Grid.cs
+++ Assets/ProjectMonster/Scripts/M_Grid.cs
@@ -6,4 +6,9 @@ public class M_Grid
 	public Vector2Int position { get; set; }
 	public GameObject gridObject { get; set; }
 
+	public M_Grid(Vector2Int inPosition, GameObject inGridObject = null)
+	{
+		position = inPosition;
+		gridObject = inGridObject;
+	}
 }
